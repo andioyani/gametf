9aa9fb67fd3d02a0235af2cd109f2d05ae5cd172
Javier Fernandez <javierfernandez1986@gmail.com>
2020-04-21 14:49:22 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase : {
+    apiKey: "AIzaSyCpomD-qMs6wQgj9CmRG3dzwC_SX6GAtKY",
+    authDomain: "tutifruttiquarantine.firebaseapp.com",
+    databaseURL: "https://tutifruttiquarantine.firebaseio.com",
+    projectId: "tutifruttiquarantine",
+    storageBucket: "tutifruttiquarantine.appspot.com",
+    messagingSenderId: "739354893018",
+    appId: "1:739354893018:web:b6784f0d870e9fec5d23e3",
+    measurementId: "G-FQBQ8H93BF"
+	}
 };
